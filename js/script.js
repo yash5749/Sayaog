99730ff8dffa07685ec25d8fd9fbf3038b55aeda
yash5749 <7yash.maurya@.com>
2025-01-09 20:27:11 +0530

    submit button popup addded

--- js/script.js
+++ js/script.js
@@ -8,3 +8,22 @@ document.querySelectorAll('.image-container img').forEach(image => {
 document.querySelector('.pop-image span').onclick = () => {
     document.querySelector('.pop-image').style.display = 'none';
 }
+function showModal() {
+    var modal = document.getElementById('successModal');
+    modal.style.display = 'block';
+  }
+
+  // Function to close the modal
+  function closeModal() {
+    var modal = document.getElementById('successModal');
+    modal.style.display = 'none';
+  }
+
+  // Close the modal if the user clicks outside of it
+  window.onclick = function(event) {
+    var modal = document.getElementById('successModal');
+    if (event.target == modal) {
+      modal.style.display = 'none';
+    }
+  }
+ 
